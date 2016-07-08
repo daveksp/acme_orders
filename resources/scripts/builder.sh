fe505c0112238fd5b111d4fb5143503efeb7d3e3
David Pinheiro <daveksp@gmail.com>
2016-07-08 00:25:14 -0300

    fix bug in builder script

--- resources/scripts/builder.sh
+++ resources/scripts/builder.sh
@@ -6,5 +6,5 @@ OLD_TAG=$(docker images | grep acme_orders | awk '{print $2}')
 TAG=`echo 1 + $OLD_TAG | bc`
 echo "$TAG"
 
-docker build -t acme_orders:$TAG -f acme_orders/resources/scripts/Dockerfile .
+docker build -t acme_orders:$TAG -f resources/scripts/Dockerfile .
 docker run -d -p 8089:8089 acme_orders:$TAG
